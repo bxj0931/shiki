3ea1599a642988fc5b13cbaf7c2b2833b97325e4
bxj0931 <bxj0931@outlook.com>
2020-06-23 21:00:35 +0800

    clear

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello git!\n");
-    printf("hello github!\n");
-    printf("hello bxj0931!\n");
-    printf("hello yuko!\n");
-    return 0;
-}
--- test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   printf("this is a git test\n");
-   return 0;
-}
